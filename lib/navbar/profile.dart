79c8e14698b6cd38bc933325839cc045865c4a2e
ninggg04 <ningming559@gmail.com>
2024-07-21 23:39:09 +0700

    Update profile.dart

--- lib/navbar/profile.dart
+++ lib/navbar/profile.dart
@@ -6,7 +6,7 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[200]
+      backgroundColor: Colors.purple[100]
       
     );
   }
